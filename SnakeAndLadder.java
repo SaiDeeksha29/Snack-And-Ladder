7235a58585ed0d1091b5ed462747480e114999f2
SaiDeeksha29 <saideeksha29@gmail.com>
2020-09-24 23:44:48 +0530

    Modified:Random Option Generated

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -12,6 +12,9 @@ public class SnakeAndLadder {
 	/**
 	 * @param args
 	 */
+	public static final int noPlay=1;
+	public static final int ladder=2;
+	public static final int snake=3;
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -21,7 +24,19 @@ public class SnakeAndLadder {
 		
 		//Computation
 		int random = (int)(Math.random() * (6) + 1);
+		int choice = (int)(Math.random() * (3) + 1);
 		
+        switch(choice) {
+        
+        case noPlay: System.out.println("The player stays in the same position:"+position);
+                     break;
+        case ladder: position=position+random;
+                     System.out.println("The player moves ahead by number of position received by the die:"+position);
+                     break;
+        case snake:  position=position-random;
+                     System.out.println("The player moves behind by number of position received by the die:"+position);
+                     break;
+        }
 	}
 
 }
