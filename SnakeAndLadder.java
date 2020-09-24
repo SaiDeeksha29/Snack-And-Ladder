fc018df8ad3f03828279e03035fc148534dcbb5b
SaiDeeksha29 <saideeksha29@gmail.com>
2020-09-24 22:52:10 +0530

    Modified:Player at zero position

---
+++ SnakeAndLadder.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package com.assignment2.training;
+
+/**
+ * @author saideeksha
+ *
+ */
+public class SnakeAndLadder {
+
+	/**
+	 * @param args
+	 */
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//variable
+		int position=0;
+		
+		System.out.println("Player started the game and is at position: "+position);
+	}
+
+}
